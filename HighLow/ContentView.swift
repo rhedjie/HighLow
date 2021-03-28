4bced244e0c9e7f445af8c92baf1fb3ca51f69c2
Rhedjie Cruz <cruzrhedjie@gmail.com>
2021-03-28 14:46:30 +0800

    Initial Commit

---
+++ HighLow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HighLow
+//
+//  Created by Rhedjie Cruz on 3/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HighLow/HighLowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighLowApp.swift
+//  HighLow
+//
+//  Created by Rhedjie Cruz on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct HighLowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
